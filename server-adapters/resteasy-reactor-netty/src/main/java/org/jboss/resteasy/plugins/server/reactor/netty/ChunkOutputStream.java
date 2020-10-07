4a4817c43459ae1cdc71031d407a5225785255be
Samuel Cox <sacox@paypal.com>
2020-10-07 15:19:35 -0500

    Use @anilgursel 's EmitterProcessor.
    I knew I was missing something when I had to create my own listener!
    
    Anyhow, I can see no negatives to EmitterProcessor and the code is
    significantly simpler because I don't have to worry about how the sink
    and the listener have to be tied together.

--- server-adapters/resteasy-reactor-netty/src/main/java/org/jboss/resteasy/plugins/server/reactor/netty/ChunkOutputStream.java
+++ server-adapters/resteasy-reactor-netty/src/main/java/org/jboss/resteasy/plugins/server/reactor/netty/ChunkOutputStream.java
@@ -3,10 +3,12 @@ package org.jboss.resteasy.plugins.server.reactor.netty;
 import org.jboss.resteasy.spi.AsyncOutputStream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import reactor.core.publisher.EmitterProcessor;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.FluxSink;
 import reactor.core.publisher.Mono;
 import reactor.core.publisher.MonoProcessor;
-import reactor.core.scheduler.Schedulers;
+import reactor.netty.http.server.HttpServerRequest;
 import reactor.netty.http.server.HttpServerResponse;
 import reactor.util.function.Tuple2;
 import reactor.util.function.Tuples;
@@ -17,7 +19,7 @@ import java.util.Arrays;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Supplier;
 
 /**
  * This is the output stream leveraged by {@link
@@ -30,96 +32,64 @@ public class ChunkOutputStream extends AsyncOutputStream {
 
    private static final Logger log = LoggerFactory.getLogger(ChunkOutputStream.class);
 
-   private static final boolean COMPLETED_SIGNAL = true;
-
    private long totalBytesSent;
-
-   /**
-    * Allows the sending of bytes to the client.
-    */
-   private final HttpServerResponse response;
-
-   /**
-    * A signal back to the 'main' Flux in {@link
-    * ReactorNettyJaxrsServer.Handler#handle(reactor.netty.http.server.HttpServerRequest,
-    * HttpServerResponse)} that writing bytes back to the client has finished.
-    */
-   private final MonoProcessor<Void> completionMono;
-
-   /**
-    * A sink that will be used to bridge bytes from RestEasy (i.e. this class)
-    * to the `Flux<byte[]>` used in sendByteArray on {@link #response}.
-    */
-   private final AtomicReference<EventListener> listener = new AtomicReference<>();
-
-   /**
-    * This is used to stop the sending of bytes until the sink and {@link #listener} used
-    * with {@link #out} has been fully established.
-    */
-   private final CompletableFuture<Boolean> sinkCreated = new CompletableFuture<>();
-
-   /**
-    * Indicates we've started sending bytes.
-    */
-   private boolean started;
 
    // TODO The use of this is very questionable!  I'm just throwing
    // this in at the very end before I try something radically different
    // for the entire adapter.
    private Duration timeout;
 
-   /**
-    * The {@link Flux<byte[]>} that is fed into {@link HttpServerResponse#sendByteArray}.
-    */
-   private Flux<Tuple2<byte[], CompletableFuture<Boolean>>> out = Flux.create(sink -> {
-      log.trace("Establishing sink and listener!");
-      listener.set(new ChunkOutputStream.EventListener() {
-         @Override
-         public void data(final byte[] bs, final CompletableFuture<Boolean> demandedSignal) {
-             if (log.isTraceEnabled()) {
-                 log.trace("Sending {} bytes", bs.length);
-             }
-             sink.next(Tuples.of(bs, demandedSignal));
-         }
-         @Override
-         public void finish() {
-             sink.complete();
-         }
-      });
-      sinkCreated.complete(COMPLETED_SIGNAL);
-   });
+    /**
+     * This is the {@link Mono} that we return from
+     * {@link ReactorNettyJaxrsServer.Handler#handle(HttpServerRequest, HttpServerResponse)}
+     */
+   private MonoProcessor<Void> completionMono;
 
-   /**
-    * The interface used to transfer data from an eventing API (i.e. {@link
-    * #asyncWrite}) to a {@link Flux} ({@link #out} in this case).  See
-    * documentation around {@link Flux#create} for more information on this.
-    */
-   interface EventListener {
-      void data(byte[] s, CompletableFuture<Boolean> demandedSignal);
-      void finish();
-   }
+    /**
+     * Indicates that we've starting sending the response bytes.
+     */
+    private volatile boolean started;
+
+    /**
+     * This is ultimately think 'sink' that we write bytes to in {@link #asyncWrite(byte[], int, int)}.
+     */
+    private FluxSink<Tuple2<byte[], CompletableFuture<Void>>> byteSink;
+
+    /**
+     * This is used to establish {@link #byteSink} upon the first writing of bytes.
+     */
+   private final Supplier<FluxSink<Tuple2<byte[], CompletableFuture<Void>>>> byteSinkSupplier;
 
    ChunkOutputStream(
        final HttpServerResponse response,
        final MonoProcessor<Void> completionMono
    ) {
-      this.response = response;
-      this.completionMono = completionMono;
+       this.completionMono = completionMono;
+       this.byteSinkSupplier = () -> {
+           final EmitterProcessor<Tuple2<byte[], CompletableFuture<Void>>> bytesEmitter = EmitterProcessor.create();
+           final Flux<byte[]> out = bytesEmitter.map(tup -> {
+                   log.trace("Submitting bytes to downstream");
+                   tup.getT2().complete(null);
+                   return tup.getT1();
+               });
+           final Flux<byte[]> actualOut = timeout != null ? out.timeout(timeout) : out;
+           response.sendByteArray(actualOut).subscribe(completionMono);
+           return bytesEmitter.sink();
+       };
    }
 
    @Override
    public void write(int b) {
-      listener.get().data(new byte[] {(byte)b}, new CompletableFuture<>());
+      byteSink.next(Tuples.of(new byte[] {(byte)b}, new CompletableFuture<>()));
    }
 
    @Override
    public void close() throws IOException {
        log.trace("Closing the ChunkOutputStream.");
-       final EventListener el = listener.get();
-       if (el != null) {
-           el.finish();
-       } else {
+       if (!started || byteSink == null) {
            Mono.<Void>empty().subscribe(completionMono);
+       } else {
+           byteSink.complete();
        }
        super.close();
    }
@@ -148,42 +118,20 @@ public class ChunkOutputStream extends AsyncOutputStream {
       return CompletableFuture.completedFuture(null);
    }
 
-
     @Override
    public CompletableFuture<Void> asyncWrite(final byte[] bs, int offset, int length) {
+        final CompletableFuture<Void> cf = new CompletableFuture<>();
         if (!started) {
+            byteSink = byteSinkSupplier.get();
             started = true;
-            Flux<byte[]> actualOut =
-                out.map(tuple -> {
-                    tuple.getT2().complete(COMPLETED_SIGNAL);
-                    return tuple.getT1();
-                }).doOnRequest(l -> log.trace("{} requested", l));
-            if (timeout != null) {
-                actualOut = actualOut.timeout(timeout);
-            }
-            response
-                .sendByteArray(
-                    actualOut
-                        .doOnNext(bb -> {
-                            totalBytesSent += bb.length;
-                            log.trace("{} total bytes sent", totalBytesSent);
-                        })
-                ).subscribe(completionMono);
         }
 
-        final CompletableFuture<Boolean> cf = new CompletableFuture<>();
-        return Mono.fromFuture(sinkCreated)
-            .map(ignore -> {
-                byte[] bytes = bs;
-                if (offset != 0 || length != bs.length) {
-                    bytes = Arrays.copyOfRange(bs, offset, offset + length);
-                }
-                listener.get().data(bytes, cf);
-                return ignore;
-            })
-            .flatMap(ignore -> Mono.fromFuture(cf))
-            .then()
-            .toFuture();
+        byte[] bytes = bs;
+        if (offset != 0 || length != bs.length) {
+            bytes = Arrays.copyOfRange(bs, offset, offset + length);
+        }
+        byteSink.next(Tuples.of(bytes, cf));
+        return cf;
    }
 
    void setTimeout(final Duration timeout) {
